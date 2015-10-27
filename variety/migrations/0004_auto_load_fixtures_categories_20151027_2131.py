745013e44893f30340f955308051af7af10c6373
martinsam <martin.sam@gmail.com>
2015-10-27 22:41:29 +0100

    migration et chargement auto des catégories

--- variety/migrations/0004_auto_load_fixtures_categories_20151027_2131.py
+++ variety/migrations/0004_auto_load_fixtures_categories_20151027_2131.py
@@ -3,6 +3,31 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+import os
+from sys import path
+from django.core import serializers
+
+
+fixture_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../fixtures'))
+fixture_filename = 'categories.json'
+
+
+def load_fixture(apps, schema_editor):
+    fixture_file = os.path.join(fixture_dir, fixture_filename)
+
+    fixture = open(fixture_file, 'rb')
+    objects = serializers.deserialize('json', fixture, ignorenonexistent=True)
+    for obj in objects:
+        obj.save()
+    fixture.close()
+
+
+def unload_fixture(apps, schema_editor):
+    "Brutally deleting all entries for this model..."
+
+    MyModel = apps.get_model("variety", "Category")
+    MyModel.objects.all().delete()
+
 
 class Migration(migrations.Migration):
 
@@ -11,4 +36,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(load_fixture, reverse_code=unload_fixture),
     ]
